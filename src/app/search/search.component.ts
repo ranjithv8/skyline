0b033a52fee695751b52f970dc3fa58f74729fc5
Ranjith <ranjith.velyetodi@klm.com>
2020-04-09 20:58:38 +0200

    bugfix: added form validations

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -3,7 +3,7 @@ import { DestinationService } from './../services/destination.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import { Observable, Subscription, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -18,7 +18,9 @@ export class SearchComponent implements OnInit, OnDestroy {
   searchForm: FormGroup;
   filteredDestinations: Observable<City[]>;
   filteredSources: Observable<City[]>;
-
+  minDate = new Date();
+  formError: string;
+  isLoading: boolean;
 
   constructor(
     private fb: FormBuilder,
@@ -34,7 +36,9 @@ export class SearchComponent implements OnInit, OnDestroy {
   }
 
   fetchCities() {
+    this.isLoading = true;
     this.destinationService.fetch().subscribe((response: Cities) => {
+      this.isLoading = false;
       this.sources = Object.values(response);
       this.destinations = Object.values(response);
       this.filteredSources = this.autoComplete(this.searchForm.get('source'), this.sources);
@@ -44,29 +48,44 @@ export class SearchComponent implements OnInit, OnDestroy {
 
   setUpForm() {
     this.searchForm = this.fb.group({
-      source: ['', Validators.required],
-      destination: ['', Validators.required],
+      source: ['', Validators.required, this.validateCity],
+      destination: ['', Validators.required, this.validateCity],
       date: [new Date(), Validators.required]
     });
   }
 
+  validateCity(control:FormControl) {
+    if(!control.value.code) {
+      return of({required:true});
+    }
+    return of(null);
+  }
+
   search() {
-    this.router.navigate(['/search-results'], {
-      queryParams: { 
-        from: this.searchForm.get('source').value.code,
-        to: this.searchForm.get('destination').value.code,
-        date: this.formatDate(this.searchForm.get('date').value)
-      }
-    });
+    if(this.searchForm.valid) {
+      this.router.navigate(['/search-results'], {
+        queryParams: { 
+          from: this.searchForm.get('source').value.code,
+          to: this.searchForm.get('destination').value.code,
+          date: this.formatDate(this.searchForm.get('date').value)
+        }
+      });
+    } else {
+      this.formError = "Please enter all highlighted fields with valid details";
+    }
   }
 
   comparefavourites() {
-    this.router.navigate(['/favourites'], { 
-      queryParams: { 
-        from: this.searchForm.get('source').value.code,
-        to: Object.keys(this.favouriteCities).join(','),
-        date: this.formatDate(this.searchForm.get('date').value)
-      } });
+    if(this.disableComparison()) {
+        this.formError = 'Please star atleast destinations from dropdown and select a source to compare';
+    } else {
+      this.router.navigate(['/favourites'], { 
+        queryParams: { 
+          from: this.searchForm.get('source').value.code,
+          to: Object.keys(this.favouriteCities).join(','),
+          date: this.formatDate(this.searchForm.get('date').value)
+        } });
+      }
   }
 
   starDestination(e, city: City) {
@@ -102,7 +121,7 @@ export class SearchComponent implements OnInit, OnDestroy {
   disableComparison() {
     return !(this.searchForm.get('source').valid
             && this.searchForm.get('date').valid
-              && Object.keys(this.favouriteCities)?.length);
+              && Object.keys(this.favouriteCities)?.length > 1);
   }
 
   formatDate(date: Date) {
